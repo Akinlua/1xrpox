d22385b4754fb15c19051622ae8536425cd1a8e3
Olamidipupo Favour <dipoagboola2019@gmail.com>
2025-03-14 19:03:01 +0100

    f

--- payooner.py
+++ payooner.py
@@ -478,6 +478,7 @@ class OTPSender:
                 options = webdriver.ChromeOptions()
                 # options.add_argument('--start-maximized')
                 options.add_argument('--headless')
+                options.add_argument('--no-sandbox')
                 options.add_argument('--disable-dev-shm-usage')
                 options.add_argument('--disable-extensions')
                 options.add_argument('--disable-gpu')
